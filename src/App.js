a840a35ceba99d0d5674bb9f2b79df4425411a5b
Wouter de Vos <wouter@codaisseur.com>
2016-07-12 14:50:48 +0200

    Store current user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Utils from './lib/Utils';
 import Header from './containers/header';
 import Channel from './containers/channel';
 import MessageInput from './containers/message-input';
@@ -8,13 +9,21 @@ class App extends React.Component {
   constructor() {
     super();
 
+    this.utils = new Utils();
+    let currentUser = this.utils.store("chat.currentUser");
+
+    if (currentUser.length === 0) { currentUser = "guest"; }
+    console.log(currentUser);
+
     this.state = {
       messages: [],
-      currentUser: "guest"
+      currentUser: currentUser
     };
   }
 
   setUsername(newUsername) {
+    this.utils.store("chat.currentUser", newUsername);
+
     this.setState({
       currentUser: newUsername
     });
